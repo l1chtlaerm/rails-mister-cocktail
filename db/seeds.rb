0511947774d56676993f4e78251d4a04d5862838
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-06-20 12:10:35 -0300

    seeded some ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create!(name: "Light rum")
+
+Ingredient.create!(name: "Applejack")
+
+Ingredient.create!(name: "Gin")
+
+Ingredient.create!(name: "Dark rum")
+
+Ingredient.create!(name: "Sweet Vermouth")
+
+Ingredient.create!(name: "Strawberry schnapps")
+
+Ingredient.create!(name: "Scotch")
+
+Ingredient.create!(name: "Apricot brandy")
+
+Ingredient.create!(name: "Triple sec")
+
+Ingredient.create!(name: "Southern Comfort")
+
+Ingredient.create!(name: "Orange bitters")
+
+Ingredient.create!(name: "Brandy")
+
+Ingredient.create!(name: "Lemon vodka")
+
+Ingredient.create!(name: "Blended whiskey")
+
+Ingredient.create!(name: "Dry Vermouth")
+
+Ingredient.create!(name: "Amaretto")
+
+Ingredient.create!(name: "Tea")
+
+Ingredient.create!(name: "Champagne")
+
+Ingredient.create!(name: "Coffee liqueur")
+
+Ingredient.create!(name: "Bourbon")
+
+Ingredient.create!(name: "Tequila")
+
+Ingredient.create!(name: "Vodka")
+
+Ingredient.create!(name: "Añejo rum")
+
+Ingredient.create!(name: "Bitters")
+
+Ingredient.create!(name: "Sugar")
+
+Ingredient.create!(name: "Kahlua")
